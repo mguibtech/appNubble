dbe6e908ccaab13ff02ba1ab6c7823f061c8946d
Marcos Guibson <mguibtech@gmail.com>
2024-08-26 20:57:36 -0400

    [FEAT] - Adding font family in app

--- App.tsx
+++ App.tsx
@@ -5,15 +5,9 @@ import {Text} from './src/components/Text/Text';
 function App(): React.JSX.Element {
   return (
     <SafeAreaView>
-      <Text preset="paragraphCaption" style={{color: 'red'}}>
-        Testando novo componente
-      </Text>
-      <Text preset="headingLarge" style={{fontFamily: 'Panchang-Light'}}>
-        Testando novo componente
-      </Text>
-      <Text preset="headingLarge" style={{fontFamily: 'Panchang-Extralight'}}>
-        Testando novo componente
-      </Text>
+      <Text preset="paragraphCaption">Testando novo componente</Text>
+      <Text preset="headingLarge">Testando novo componente</Text>
+      <Text preset="paragraphCaption">Testando novo componente</Text>
     </SafeAreaView>
   );
 }
