3e4c29ace45acb41c5eac90366885dc31db892f9
Marcos Guibson <mguibtech@gmail.com>
2024-09-23 22:26:59 -0400

    [feat] - Adding TextInput component

--- App.tsx
+++ App.tsx
@@ -1,15 +1,40 @@
 import React from 'react';
-import {SafeAreaView} from 'react-native';
+import {SafeAreaView, View} from 'react-native';
 import {ThemeProvider} from '@shopify/restyle';
 import {theme} from './src/theme/theme';
+import {Text} from './src/components/Text/Text';
+import {Button} from './src/components/Button/Button';
+import {TextInput} from './src/components/TextInput/TextInput';
 import {Icon} from './src/components/Icon/Icon';
 
 function App(): React.JSX.Element {
   return (
     <ThemeProvider theme={theme}>
       <SafeAreaView>
-        <Icon name="arrowRight" />
-        <Icon name="eyeOn" color="buttonPrimary" size={50} />
+        <View style={{paddingHorizontal: 24}}>
+          <Text preset="headingLarge" mb="s8">
+            Ola!
+          </Text>
+          <Text preset="paragraphLarge" mb="s40">
+            Digite seu e-mail e senha para entrar
+          </Text>
+
+          <TextInput
+            boxProps={{
+              mb: 's10',
+            }}
+            label="E-mail"
+          />
+          <TextInput
+            label="Senha"
+            rightComponent={<Icon name="eyeOff" />}
+            errorMessage="Testando"
+          />
+          <Text>Esqueci minha senha</Text>
+
+          <Button preset="primary" title="Entrar" mt="s48" />
+          <Button preset="outline" title="Criar uma conta" mt="s12" />
+        </View>
       </SafeAreaView>
     </ThemeProvider>
   );
---
+++ src/components/TextInput/TextInput.tsx
@@ -0,0 +1,75 @@
+import React, {useRef} from 'react';
+import {
+  Pressable,
+  TextInput as RNTextInput,
+  TextInputProps as RNTextInputProps,
+  TextStyle,
+} from 'react-native';
+import {useAppTheme} from '../../hooks/useAppTheme';
+import {Box, BoxProps} from '../Box/Box';
+import {$fontFamily, $fontSizes, Text} from '../Text/Text';
+
+interface TextInputProps extends RNTextInputProps {
+  label: string;
+  errorMessage?: string;
+  rightComponent?: React.ReactElement;
+  boxProps?: BoxProps;
+}
+export function TextInput({
+  label,
+  errorMessage,
+  rightComponent,
+  boxProps,
+  ...rnTextInputProps
+}: TextInputProps) {
+  const {colors} = useAppTheme();
+  const inputRef = useRef<RNTextInput>(null);
+
+  const $textInputContainer: BoxProps = {
+    flexDirection: 'row',
+    borderWidth: errorMessage ? 2 : 1,
+    borderColor: errorMessage ? 'error' : 'gray4',
+    padding: 's16',
+    borderRadius: 's12',
+  };
+
+  function focusInput() {
+    inputRef.current?.focus();
+  }
+
+  return (
+    <Box {...boxProps}>
+      <Pressable onPress={focusInput}>
+        <Text preset="paragraphMedium" marginBottom="s4">
+          {label}
+        </Text>
+        <Box {...$textInputContainer}>
+          <RNTextInput
+            ref={inputRef}
+            placeholderTextColor={colors.gray2}
+            style={$textInputStyle}
+            {...rnTextInputProps}
+          />
+          {rightComponent && (
+            <Box ml="s16" justifyContent="center">
+              {rightComponent}
+            </Box>
+          )}
+        </Box>
+        {errorMessage && (
+          <Text color="error" bold preset="paragraphSmall">
+            {errorMessage}
+          </Text>
+        )}
+      </Pressable>
+    </Box>
+  );
+}
+
+const $textInputStyle: TextStyle = {
+  flexGrow: 1,
+  flexShrink: 1,
+  padding: 0,
+  fontFamily: $fontFamily.regular,
+  ...$fontSizes.paragraphMedium,
+};
